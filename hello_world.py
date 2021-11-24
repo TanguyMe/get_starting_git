134f909c3f85a54df7164a1814eb3918ebd9e947
Arthur Telders <atelders@gmail.com>
2021-11-24 15:10:17 +0100

    change function

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,7 @@
 def hello(prenom):
     if prenom == "Tanguy":
         print("Hello Tanguy !")
+    elif prenom == "Arthur":
+        print("Hello Arthur !")
     else:
         print("Je ne vous connais pas.")
